114717159ef84a65111ce8bf56281c3286184f26
Astha Singh <astha.singh@nagarro.com>
2018-02-27 00:20:36 +0530

    first commit

---
+++ src/Validatable.java
@@ -0,0 +1,5 @@
+
+public interface Validatable {
+
+	public boolean isValid(Validator validator);
+}
---
+++ src/Validator.java
@@ -0,0 +1,8 @@
+
+public interface Validator {
+
+	public boolean validate(BattleField battleField);
+	public boolean validate(Cell cell);
+	public boolean validate(Game game);
+	
+}
